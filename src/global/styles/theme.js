b999574e306e72e898fe3c0ece1dfc46ecd4e8c8
Felipe Damasceno <felipe@superempresa.com.br>
2021-08-11 10:34:44 -0300

    theme, splash screen, fonts

---
+++ src/global/styles/theme.js
@@ -0,0 +1,21 @@
+export const theme = {
+    colors: {
+        primary: '#46D0D9',
+        secondary: '#13497B',
+        background: '#F8FBFF',
+        
+        heading: '#143656',
+        heading2: '#13497B',
+
+        text: '#000000',
+        text2: '#D3DBE2',
+
+        red: '#E83F67',
+        green: '#6CC775'
+    },
+    fonts: {
+        text500: 'Rubik_500Medium',
+        text400: 'Rubik_400Regular',
+        text300: 'Rubik_300Light',
+    }
+}
